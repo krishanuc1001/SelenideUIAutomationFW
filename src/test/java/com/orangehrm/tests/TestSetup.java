12df589f74f7171c5ae39eec6a0e31b36e9343b6
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2023-10-02 01:22:22 +0530

    Add annotation to implement Selenide Text report

--- src/test/java/com/orangehrm/tests/TestSetup.java
+++ src/test/java/com/orangehrm/tests/TestSetup.java
@@ -1,9 +1,12 @@
 package com.orangehrm.tests;
 
+import com.codeborne.selenide.junit5.TextReportExtension;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static com.codeborne.selenide.Selenide.open;
 
+@ExtendWith({TextReportExtension.class})
 public class TestSetup {
 
     @BeforeEach
