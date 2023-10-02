0087772ac4289bb8456c0d66591a8c1f86a21c2d
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2023-10-03 01:53:47 +0530

    Add setUp and tearDown for generating Allure reports with Selenide

--- src/test/java/com/orangehrm/tests/TestSetup.java
+++ src/test/java/com/orangehrm/tests/TestSetup.java
@@ -1,16 +1,45 @@
 package com.orangehrm.tests;
 
 import com.codeborne.selenide.junit5.TextReportExtension;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static com.codeborne.selenide.Selenide.open;
 
 @ExtendWith({TextReportExtension.class})
 public class TestSetup {
 
+    @BeforeAll
+    static void setupAllureReports() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true));
+    }
+
     @BeforeEach
     void setUp() {
         open("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
     }
+
+    @AfterAll
+    static void tearDownAllureReports() throws IOException {
+        if (Files.exists(Path.of("target/allure-results"))) {
+            try {
+                ProcessBuilder builder = new ProcessBuilder("mvn", "allure:serve");
+                builder.inheritIO();
+                Process process = builder.start();
+                process.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
