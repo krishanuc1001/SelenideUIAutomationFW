44b88aab04e21a0fc3241b9cf780d5b12d5ee5c1
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2023-10-04 08:22:35 +0530

    Remove unused imports from TestSetup class

--- src/test/java/com/orangehrm/tests/TestSetup.java
+++ src/test/java/com/orangehrm/tests/TestSetup.java
@@ -3,14 +3,9 @@ package com.orangehrm.tests;
 import com.codeborne.selenide.junit5.TextReportExtension;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static com.codeborne.selenide.Selenide.open;
 
